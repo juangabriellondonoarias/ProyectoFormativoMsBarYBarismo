75621e71751ce98a43fad4adbe31f555b0e29956
juangabriellondonoarias <juangabriellondonoarias@gmail.com>
2025-12-05 22:06:30 -0500

    Inicio de proyecto

---
+++ src/main/java/com/example/demo/MsBarYBarismoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBarYBarismoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBarYBarismoApplication.class, args);
+	}
+
+}
